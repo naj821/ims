c7f8feadcf514c85cf7341a5a0424b55b823db16
sirezo <joebertcerezo@gmail.com>
2025-06-12 16:16:16 +0800

    Update AuthController.kt

--- src/main/kotlin/com/vauldex/inventory_management/controller/AuthController.kt
+++ src/main/kotlin/com/vauldex/inventory_management/controller/AuthController.kt
@@ -1,21 +1,29 @@
 package com.vauldex.inventory_management.controller
 
-import com.vauldex.inventory_management.domain.dto.request.UserResquest
+import com.vauldex.inventory_management.domain.dto.request.UserRequest
 import com.vauldex.inventory_management.domain.dto.response.UserResponse
 import com.vauldex.inventory_management.service.abstraction.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.slf4j.LoggerFactory
 
 @RestController
 @RequestMapping("/api/sessions")
-class AuthController(private  val userService: UserService) {
+class AuthController(private val userService: UserService) {
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
-    @PostMapping
-    fun create(user: UserResquest): ResponseEntity<UserResponse> {
-       val user =  userService.verifyPassword(user.toEntity())
-        return ResponseEntity.status(HttpStatus.CREATED).body(user)
+    @PostMapping("/dd")
+    fun create(@RequestBody user: UserRequest): ResponseEntity<UserResponse> {
+      logger.info("*".repeat(100))
+      logger.info("aas")
+      logger.info("*".repeat(100))
+      val user1 = userService.create(user.toEntity())
+
+      return ResponseEntity.status(HttpStatus.CREATED).body(user1)
     }
+
 }
