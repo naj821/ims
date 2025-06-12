6f4e85ca6bb5a0a76dc0dbf787af7e1894184879
sirezo <joebertcerezo@gmail.com>
2025-06-12 16:16:32 +0800

    Update UserServiceImpl.kt

--- src/main/kotlin/com/vauldex/inventory_management/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/com/vauldex/inventory_management/service/impl/UserServiceImpl.kt
@@ -8,11 +8,17 @@ import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(private val userRepo: UserRepository): UserService {
-    override fun verifyPassword(user: UserEntity): UserResponse {
-        val doesExists = userRepo.existByEmailAndPassword(email = user.email, password = user.password)
-        if(!doesExists) throw Exception("Invalid credentials.")
+    // override fun verifyPassword(user: UserEntity): UserResponse {
+    //   val doesExists = userRepo.existByEmailAndPassword(email = user.email, password = user.password)
+    //   if(!doesExists) throw Exception("Invalid credentials.")
 
-        val user = userRepo.findByEmailAndPassword(email = user.email, password = user.password)
-        return user.toResponse()
+    //   val foundUser = userRepo.findByEmailAndPassword(email = user.email, password = user.password)
+    //   return foundUser.toResponse()
+    // }
+
+    override fun create(user: UserEntity): UserResponse {
+      println(user)
+      val user = userRepo.save(user)
+      return user.toResponse()
     }
 }
