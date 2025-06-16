b6d95a2d9cff2c1988fd46455c40c61efdcf2d8f
sirezo <joebertcerezo@gmail.com>
2025-06-16 08:03:52 +0800

    Create UserController.kt

---
+++ src/main/kotlin/com/vauldex/inventory_management/controller/UserController.kt
@@ -0,0 +1,26 @@
+package com.vauldex.inventory_management.controller
+
+import com.vauldex.inventory_management.domain.dto.request.UserCreateRequest
+import com.vauldex.inventory_management.domain.dto.response.UserResponse
+import com.vauldex.inventory_management.response.ResponseSuccess
+import com.vauldex.inventory_management.service.abstraction.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/users")
+class UserController(private  val userService: UserService) {
+
+    @PostMapping
+    fun create(@RequestBody user: UserCreateRequest): ResponseSuccess<UserResponse> {
+       val userResponse =  userService.create(user)
+        return ResponseSuccess(
+            code = "USER_CREATED",
+            status = HttpStatus.CREATED,
+            data = userResponse
+        )
+    }
+}
