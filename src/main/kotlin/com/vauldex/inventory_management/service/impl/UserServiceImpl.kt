dfefbf93b363d8c41a4b42ffbf6447b9898dacea
sirezo <joebertcerezo@gmail.com>
2025-06-16 08:04:04 +0800

    Update UserServiceImpl.kt

--- src/main/kotlin/com/vauldex/inventory_management/service/impl/UserServiceImpl.kt
+++ src/main/kotlin/com/vauldex/inventory_management/service/impl/UserServiceImpl.kt
@@ -1,5 +1,8 @@
 package com.vauldex.inventory_management.service.impl
 
+import com.vauldex.inventory_management.domain.dto.request.UserCreateRequest
+import com.vauldex.inventory_management.domain.dto.request.UserLoginRequest
+import com.vauldex.inventory_management.domain.dto.request.UserResquest
 import com.vauldex.inventory_management.domain.dto.response.UserResponse
 import com.vauldex.inventory_management.domain.entity.UserEntity
 import com.vauldex.inventory_management.repository.UserRepository
@@ -8,12 +11,21 @@ import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(private val userRepo: UserRepository): UserService {
-    override fun authenticate(user: UserEntity): UserResponse {
+    override fun authenticate(user: UserLoginRequest): UserResponse {
         val doesExists = userRepo.existsByEmailAndPassword(email = user.email, password = user.password)
 
         if(!doesExists) throw IllegalArgumentException("Invalid Credentials")
 
-        val user = userRepo.findByEmailAndPassword(email = user.email, password = user.password)
-        return user.toResponse()
+        val userResponse = userRepo.findByEmailAndPassword(email = user.email, password = user.password)
+        return userResponse.toResponse()
+    }
+
+    override fun create(user: UserCreateRequest): UserResponse {
+        val doesUserExist = userRepo.existsByEmail(email = user.email)
+
+        if(doesUserExist) throw IllegalArgumentException("User already exist")
+
+        val userResponse = userRepo.save(user.toEntity())
+        return userResponse.toResponse()
     }
 }
