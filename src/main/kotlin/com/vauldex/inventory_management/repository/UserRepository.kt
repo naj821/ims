9dde0ecfcd0a41d3c6f1026261d5d18c8890638b
sirezo <joebertcerezo@gmail.com>
2025-06-16 08:03:59 +0800

    Update UserRepository.kt

--- src/main/kotlin/com/vauldex/inventory_management/repository/UserRepository.kt
+++ src/main/kotlin/com/vauldex/inventory_management/repository/UserRepository.kt
@@ -9,4 +9,5 @@ import java.util.UUID
 interface UserRepository: JpaRepository<UserEntity, UUID> {
     fun existsByEmailAndPassword(email: String, password: String): Boolean
     fun findByEmailAndPassword(email: String, password: String): UserEntity
+    fun existsByEmail(email: String): Boolean
 }
