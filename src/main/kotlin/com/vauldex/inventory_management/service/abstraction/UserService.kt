7ceb74c2818e5c018f1355054483347a3cefc0ae
sirezo <joebertcerezo@gmail.com>
2025-06-18 10:18:43 +0800

    Update UserService.kt

--- src/main/kotlin/com/vauldex/inventory_management/service/abstraction/UserService.kt
+++ src/main/kotlin/com/vauldex/inventory_management/service/abstraction/UserService.kt
@@ -3,8 +3,10 @@ package com.vauldex.inventory_management.service.abstraction
 import com.vauldex.inventory_management.domain.dto.request.UserCreateRequest
 import com.vauldex.inventory_management.domain.dto.request.UserLoginRequest
 import com.vauldex.inventory_management.domain.dto.response.UserResponse
+import java.util.UUID
 
 interface UserService {
     fun authenticate(user: UserLoginRequest): UserResponse
     fun create(user: UserCreateRequest): UserResponse
+    fun find(idUser: UUID): UserResponse
 }
