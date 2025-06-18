5a49eebc4d7f2d3a64e09c8623d630cf1f673ff3
sirezo <joebertcerezo@gmail.com>
2025-06-18 10:18:42 +0800

    Update UserSessionController.kt

--- src/main/kotlin/com/vauldex/inventory_management/controller/UserSessionController.kt
+++ src/main/kotlin/com/vauldex/inventory_management/controller/UserSessionController.kt
@@ -3,6 +3,8 @@ package com.vauldex.inventory_management.controller
 import com.vauldex.inventory_management.domain.dto.request.TokenRequest
 import com.vauldex.inventory_management.domain.dto.request.UserLoginRequest
 import com.vauldex.inventory_management.domain.dto.response.LoginResponse
+import com.vauldex.inventory_management.domain.dto.response.UserResponse
+import com.vauldex.inventory_management.domain.entity.ProductEntity
 import com.vauldex.inventory_management.domain.entity.TokenEntity
 import com.vauldex.inventory_management.response.ResponseSuccess
 import com.vauldex.inventory_management.service.abstraction.AuthenticationService
@@ -14,17 +16,21 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import com.vauldex.inventory_management.utility.HashEncoder
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.bind.annotation.CookieValue
+import org.springframework.web.bind.annotation.GetMapping
+import java.util.UUID
 
 @RestController
 @RequestMapping("/api/sessions")
 class UserSessionController(
         private val userService: UserService,
         private val jwtUtils: JwtUtils,
-        private val authenticationService: AuthenticationService,
-        private val hash: HashEncoder
+        private val authService: AuthenticationService
 ) {
     @PostMapping
-    fun create(@RequestBody userLogin: UserLoginRequest): ResponseSuccess<LoginResponse> {
+    fun create(@RequestBody userLogin: UserLoginRequest, response: HttpServletResponse): ResponseSuccess<UserResponse> {
         val userResponse = userService.authenticate(userLogin)
         
         val accessToken = jwtUtils.generateAccessToken(userId = userResponse.id.toString())
@@ -36,20 +42,25 @@ class UserSessionController(
                 hashedAccessToken = accessToken,
                 hashedRefreshToken = refreshToken
         )
-        authenticationService.saveTokens(authTokenRequest.toEntity())
+        authService.saveTokens(authTokenRequest.toEntity())
+
+        val cookie = Cookie("jwt", accessToken)
+        cookie.isHttpOnly = true
+
+        response.addCookie(cookie)
 
         val loginResponse = LoginResponse(userResponse = userResponse, authorization = authTokenRequest)
 
         return ResponseSuccess(
             code = "USER_LOGIN",
             status = HttpStatus.CREATED,
-            data = loginResponse
+            data = userResponse
         )
     }
 
     @PostMapping("/refresh")
     fun refreshToken(@RequestBody refreshTokenRequest: TokenRequest): ResponseSuccess<TokenEntity> {
-        val data = authenticationService.refresh(refreshTokenRequest)
+        val data = authService.refresh(refreshTokenRequest)
 
         val response = ResponseSuccess(code = "REFRESH_TOKEN",
                 status = HttpStatus.CREATED,
@@ -57,6 +68,20 @@ class UserSessionController(
         )
 
         return response
+    }
+
+    @GetMapping
+    fun find(@CookieValue("jwt") jwt: String): ResponseSuccess<UserResponse> {
+        authService.validateAccessToken(jwt)
+      
+        val userId = jwtUtils.getUserIdFromToken(jwt)
+        val userResponse = userService.find(UUID.fromString(userId))
+
+        return ResponseSuccess(
+            code = "USER_FOUND",
+            status = HttpStatus.OK,
+            data = userResponse
+        )
     }
 
 }
