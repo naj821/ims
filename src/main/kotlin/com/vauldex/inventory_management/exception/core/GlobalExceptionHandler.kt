36e9cb7a22616003a93062f27be34fa89e42dd4b
sirezo <joebertcerezo@gmail.com>
2025-06-19 08:02:06 +0800

    Update GlobalExceptionHandler.kt

--- src/main/kotlin/com/vauldex/inventory_management/exception/core/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/vauldex/inventory_management/exception/core/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.vauldex.inventory_management.exception.core
 
+import com.vauldex.inventory_management.response.ResponseError
 import io.jsonwebtoken.ExpiredJwtException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -9,6 +10,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception::class)
+    fun handleGenericException(ex: Exception): ResponseEntity<ResponseError> {
+        val response = ResponseError(
+            code = ex.message,
+            status = HttpStatus.INTERNAL_SERVER_ERROR
+        )
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
+    }
 
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleInvalidCredentials(ex: IllegalArgumentException): ResponseEntity<GlobalErrorMessageModel> {
@@ -23,7 +33,7 @@ class GlobalExceptionHandler {
     fun handleExpireJwtToken(ex: ExpiredJwtException): ResponseEntity<ExpiredJwtTokenModel> {
         val errorMsg = ExpiredJwtTokenModel(
                 code = ex.message,
-                status = HttpStatus.UNAUTHORIZED
+                status = HttpStatus.BAD_REQUEST
         )
         return ResponseEntity(errorMsg, HttpStatus.BAD_REQUEST)
     }
@@ -32,7 +42,7 @@ class GlobalExceptionHandler {
     fun handleMissingRequestCookie(ex: MissingRequestCookieException): ResponseEntity<MissingRequestExceptionModel> {
         val errorMsg = MissingRequestExceptionModel(
                 code = ex.message,
-                status = HttpStatus.UNAUTHORIZED
+                status = HttpStatus.BAD_REQUEST
         )
 
         return ResponseEntity(errorMsg, HttpStatus.BAD_REQUEST)
