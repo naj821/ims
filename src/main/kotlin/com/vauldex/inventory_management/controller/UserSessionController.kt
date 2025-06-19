14af2cf6f52027f19a9793d01f648dccbf74da16
sirezo <joebertcerezo@gmail.com>
2025-06-19 08:02:05 +0800

    Update UserSessionController.kt

--- src/main/kotlin/com/vauldex/inventory_management/controller/UserSessionController.kt
+++ src/main/kotlin/com/vauldex/inventory_management/controller/UserSessionController.kt
@@ -6,12 +6,12 @@ import com.vauldex.inventory_management.response.ResponseSuccess
 import com.vauldex.inventory_management.service.abstraction.AuthenticationService
 import com.vauldex.inventory_management.service.abstraction.UserService
 import com.vauldex.inventory_management.utility.JwtUtils
+import com.vauldex.inventory_management.utility.CookieUtils
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -23,6 +23,7 @@ import java.util.UUID
 class UserSessionController(
         private val userService: UserService,
         private val jwtUtils: JwtUtils,
+        private val cookieUtils: CookieUtils,
         private val authService: AuthenticationService
 ) {
     @PostMapping
@@ -30,12 +31,7 @@ class UserSessionController(
         val userResponse = userService.authenticate(userLogin)
         val accessToken = userResponse.authorization.hashedAccessToken
 
-        val cookie = Cookie("jwt", accessToken)
-        cookie.isHttpOnly = true
-        cookie.secure = true
-        cookie.path = "/"
-        cookie.maxAge = 15 * 60 
-
+        val cookie = cookieUtils.createJwtCookie(accessToken)
         response.addCookie(cookie)
 
         return ResponseSuccess(
@@ -49,22 +45,19 @@ class UserSessionController(
     fun refresh(@CookieValue("jwt") jwt: String, response: HttpServletResponse) : ResponseSuccess<Unit> {
         val data = authService.refresh(jwt)
 
-        val cookie = Cookie("jwt", data)
-        cookie.isHttpOnly = true
-
+        val cookie = cookieUtils.createJwtCookie(data)
         response.addCookie(cookie)
 
-        val response = ResponseSuccess(
-                code = "REFRESH_TOKEN",
-                status = HttpStatus.CREATED,
-                data = Unit
+        return ResponseSuccess(
+            code = "REFRESH_TOKEN",
+            status = HttpStatus.CREATED,
+            data = Unit
         )
-        return response
     }
 
     @GetMapping
     fun find(@CookieValue("jwt") jwt: String?): ResponseSuccess<UserResponse> {
-        if(jwt == null) throw IllegalArgumentException("Invalid token.")
+        if(jwt == null) throw IllegalArgumentException("Invalid token.1")
 
         authService.validateAccessToken(jwt)
 
@@ -72,20 +65,23 @@ class UserSessionController(
         val userResponse = userService.find(UUID.fromString(userId))
 
         return ResponseSuccess(
-            code = "USER_FOUND",
+            code = "USER_SESSION_FOUND",
             status = HttpStatus.OK,
             data = userResponse
         )
     }
 
     @DeleteMapping
-    fun delete(@CookieValue("jwt") jwt: String): ResponseSuccess<Unit> {
+    fun delete(@CookieValue("jwt") jwt: String, response: HttpServletResponse): ResponseSuccess<Unit> {
+        val cookie = cookieUtils.createExpiredJwtCookie()
+        response.addCookie(cookie)
+
         val data = userService.logout(jwt)
 
         return ResponseSuccess(
-                code = "USER_LOGOUT",
-                status = HttpStatus.OK,
-                data = data
+            code = "USER_LOGOUT",
+            status = HttpStatus.OK,
+            data = data
         )
     }
 
