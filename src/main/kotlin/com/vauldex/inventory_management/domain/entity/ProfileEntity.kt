ef496799df515a0f7c29d5d3ca1a479ebd2e42aa
sirezo <joebertcerezo@gmail.com>
2025-06-20 10:24:16 +0800

    Update ProfileEntity.kt

--- src/main/kotlin/com/vauldex/inventory_management/domain/entity/ProfileEntity.kt
+++ src/main/kotlin/com/vauldex/inventory_management/domain/entity/ProfileEntity.kt
@@ -1,8 +1,11 @@
 package com.vauldex.inventory_management.domain.entity
 
+import com.vauldex.inventory_management.domain.dto.response.ProfileReponse
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
@@ -15,16 +18,26 @@ import java.util.UUID
 @Table(name = "profiles")
 class ProfileEntity (
         @Column(name ="first_name")
-        var firstName: String,
+        var firstName: String = "",
         @Column(nullable = false)
-        var middleName: String,
+        var middleName: String = "",
         @Column(name = "last_name")
-        var lastName: String,
+        var lastName: String = "",
         @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        val id: UUID? = null,
         @OneToOne(cascade = [CascadeType.REMOVE])
         @JoinColumn(name = "user_id")
-        val userEntity: UserEntity,
+        val userEntity: UserEntity? = null,
         @Column(name = "created_at")
         @CreationTimestamp
         var createdAt: LocalDateTime? = null
-)
+) {
+        fun toResponse(): ProfileReponse = ProfileReponse(
+                id = userEntity?.id,
+                firstName = firstName,
+                middleName = middleName,
+                lastName = lastName,
+                createdAt = createdAt
+        )
+}
